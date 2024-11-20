887b6938508cd5bcdaf0bfbf9bf60bf0db93f84c
Soraya <sorayagarcesfernandez@gmail.com>
2024-11-20 22:37:39 +0100

    Modificación realizada por Soraya Garcés

--- Ej2Te3.java
+++ Ej2Te3.java
@@ -1,4 +1,4 @@
-/*Diseña 2 funciones: la 1ª calculará y devolverá la superficie de una esfera; la 2ª calculará y
+/*Diseña 2ºº funciones: la 1ª calculará y devolverá la superficie de una esfera; la 2ª calculará y
 devolverá su volumen. Ambas funciones recibirán como parámetros el radio (tipo real). (RA4)
  */
 import java.util.Scanner;
@@ -9,13 +9,15 @@ public class Ej2Te3 {
     }
 
     static double volEsf(double r) {
-        return 4*Math.PI/3*Math.pow(r,3);
+        return (4.0 / 3.0) * Math.PI * Math.pow(r, 3); // Se modifica la fórmula por Soraya Garcés
+        //return 4*Math.PI/3*Math.pow(r,3);  de Alexis
     }
 
     public static void main(String[] args) {
          double r;
          Scanner sc = new Scanner(System.in);
-         System.out.print("Radio ");
+         System.out.print("\nIntroduce el valor del Radio: "); //Se modifica la entrada de texto, por Soraya Garcés
+        //System.out.print("Radio: "); de Alexis
          r = sc.nextDouble();
          System.out.println("Superficie: " + supEsf(r));
          System.out.println("Volumen: " + volEsf(r));
---
+++ out/production/ed-ut5-Alexis-Valero-Cremades/README.md
@@ -0,0 +1,3 @@
+# ed-ut5-Alexis-Valero-Cremades
+
+Pequeña aplicación En Java para la práctica de la asignatura de Entornos de Desarrollo
